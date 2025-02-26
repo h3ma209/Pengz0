070fa849921bab35903f8174a734b972117c7499
h3ma209 <hema.omer007@gmail.com>
2025-02-26 20:46:51 +0300

    beg

---
+++ src/bitmaps/dedsec.h
@@ -0,0 +1,8 @@
+#ifndef DEDSEC_H
+#define DEDSEC_H
+
+#include <stdint.h>
+
+extern const uint8_t dedsec[1024];  // Declare the bitmap as an external variable
+
+#endif // DEDSEC_H
---
+++ src/bitmaps/kawaski.h
@@ -0,0 +1,8 @@
+#ifdef KAWASKI_H
+#define KAWASKI_H
+
+#include <stdint.h>
+
+extern const uint8_t kawaskiBitmap[1024];  // Declare the bitmap as an external variable
+
+#endif // KAWASKI_H
